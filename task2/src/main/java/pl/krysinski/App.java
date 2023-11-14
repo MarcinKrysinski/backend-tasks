511be91cfa9cc7bad1c28a29d2085c41d8fb59c4
Marcin Krysiński <marcin.jakubkrysinski@gmail.com>
2023-11-14 19:39:33 +0100

    added solution task2

--- task2/src/main/java/pl/krysinski/App.java
+++ task2/src/main/java/pl/krysinski/App.java
@@ -1,13 +1,38 @@
 package pl.krysinski;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] numbers = input.split(" ");
+        int[] integerArray = getIntegerArray(numbers);
+        List<String> result = findPairs(integerArray);
+        result.stream().sorted().forEach(System.out::println);
+        scanner.close();
+    }
+
+    public static int[] getIntegerArray(String[] numbers) {
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+    }
+
+    private static List<String> findPairs(int[] numbersArray) {
+        List<String> result = new ArrayList<>();
+        Set<Integer> checkedNumbers = new HashSet<>();
+        for (int num : numbersArray) {
+            int complement = 13 - num;
+            if (checkedNumbers.contains(complement)) {
+                int smaller = Math.min(num, complement);
+                int larger = Math.max(num, complement);
+                result.add(smaller + " " + larger);
+            }
+            checkedNumbers.add(num);
+        }
+        return result;
     }
 }
