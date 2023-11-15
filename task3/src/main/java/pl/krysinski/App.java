c88e640985d7dbd2334365005be94ce1d78b95bd
Marcin Krysiński <marcin.jakubkrysinski@gmail.com>
2023-11-15 13:55:37 +0100

    refactoring bfs algorithm, added tests, added menu

--- task3/src/main/java/pl/krysinski/App.java
+++ task3/src/main/java/pl/krysinski/App.java
@@ -6,36 +6,59 @@ public class App {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
+        while (true) {
+            System.out.println("Enter number of connections (or 'q' to quit): ");
+            if (scanner.hasNextInt()) {
+                int connectionsNumber = scanner.nextInt();
+                Map<Integer, Set<Integer>> graphConnectionsMap = getGraphConnectionsMap(connectionsNumber, scanner);
+                if (!graphConnectionsMap.isEmpty()) {
+                    int separatedGraphs = countSeparatedGraphs(graphConnectionsMap);
+                    System.out.println(separatedGraphs);
+                }
+            } else {
+                String userInput = scanner.next();
+                if (userInput.equalsIgnoreCase("q")) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please enter a valid integer or 'q' to quit.");
+                }
+            }
+        }
 
-        Map<Integer, ArrayList<Integer>> graphConnections = getGraphConnections(n, scanner);
-
-        int numberSeparatedGraphs = countSeparatedGraphs(graphConnections);
-        System.out.println(numberSeparatedGraphs);
         scanner.close();
+
     }
 
-    static Map<Integer, ArrayList<Integer>> getGraphConnections(int n, Scanner scanner) {
-        Map<Integer, ArrayList<Integer>> graphConnections = new HashMap<>();
+    static Map<Integer, Set<Integer>> getGraphConnectionsMap(int connectionsNumber, Scanner scanner) {
+        Map<Integer, Set<Integer>> graphConnectionsMap = new HashMap<>();
+        System.out.println("Enter " + connectionsNumber + " lines of pair connections seperated by a space \n(example:\n1 2\n2 3): ");
+        for (int i = 0; i < connectionsNumber; i++) {
+            try {
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
-
-            graphConnections.computeIfAbsent(a, neighbour -> new ArrayList<>()).add(b);
-            graphConnections.computeIfAbsent(b, neighbour -> new ArrayList<>()).add(a);
+                graphConnectionsMap.computeIfAbsent(a, neighbour -> new HashSet<>()).add(b);
+                graphConnectionsMap.computeIfAbsent(b, neighbour -> new HashSet<>()).add(a);
+            } catch (InputMismatchException e) {
+                System.out.println("Input is not a integer value.");
+                scanner.nextLine();
+                graphConnectionsMap.clear();
+                return graphConnectionsMap;
+            }
 
         }
-        return graphConnections;
+        return graphConnectionsMap;
     }
 
-    static int countSeparatedGraphs(Map<Integer, ArrayList<Integer>> graphConnections) {
-        ArrayList<Integer> visitedElement = new ArrayList<>();
+    static int countSeparatedGraphs(Map<Integer, Set<Integer>> graphConnections) {
         int separatedGraphs = 0;
+        if (graphConnections.isEmpty()) {
+            return separatedGraphs;
+        }
+        ArrayList<Integer> visitedElement = new ArrayList<>();
 
         for (int vertex : graphConnections.keySet()) {
-            if (!visitedElement.contains(vertex)) {
-                breadthFirstSearchAlgorithm(graphConnections, vertex, visitedElement);
+            if (!visitedElement.contains(vertex) && breadthFirstSearchAlgorithm(graphConnections, vertex, visitedElement)) {
                 separatedGraphs++;
             }
         }
@@ -43,20 +66,22 @@ public class App {
         return separatedGraphs;
     }
 
-    private static void breadthFirstSearchAlgorithm(Map<Integer, ArrayList<Integer>> graphConnections, int startVertex, ArrayList<Integer> visitedElement) {
+    private static boolean breadthFirstSearchAlgorithm(Map<Integer, Set<Integer>> graphConnections, int startVertex, ArrayList<Integer> visitedElement) {
         Queue<Integer> queue = new LinkedList<>();
         queue.add(startVertex);
 
         while (!queue.isEmpty()) {
             int currentVertex = queue.poll();
-            for (int neighbor : graphConnections.get(currentVertex)) {
+            for (int neighbor : graphConnections.getOrDefault(currentVertex, Collections.emptySet())) {
                 if (!visitedElement.contains(neighbor)) {
-                    if (graphConnections.size() % 2 == 0) {
-                        queue.add(neighbor);
+                    if (currentVertex == neighbor) {
+                        return false;
                     }
+                    queue.add(neighbor);
                     visitedElement.add(neighbor);
                 }
             }
         }
+        return true;
     }
 }
--- task3/src/test/java/pl/krysinski/AppTest.java
+++ task3/src/test/java/pl/krysinski/AppTest.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
     @Test
-    void testCountGraphs2() {
+    void testCountSeparatedGraphs() {
         //given
-        Map<Integer, ArrayList<Integer>> graph2 = new HashMap<>();
-        graph2.put(1, new ArrayList<>(List.of(3, 3)));
-        graph2.put(3, new ArrayList<>(List.of(1, 1)));
-        graph2.put(5, new ArrayList<>(List.of(6)));
-        graph2.put(6, new ArrayList<>(List.of(5)));
-        graph2.put(7, new ArrayList<>(List.of(8)));
+        Map<Integer, Set<Integer>> graph2 = new HashMap<>();
+        graph2.put(1, new HashSet<>(Set.of(3)));
+        graph2.put(3, new HashSet<>(Set.of(1)));
+        graph2.put(5, new HashSet<>(Set.of(6)));
+        graph2.put(6, new HashSet<>(Set.of(5)));
+        graph2.put(7, new HashSet<>(Set.of(8)));
         //when
         int result = App.countSeparatedGraphs(graph2);
         //then
@@ -25,18 +25,34 @@ public class AppTest {
     }
 
     @Test
-    void testGetGraph2() {
+    void testGetGraphConnectionsMap() {
         //given
         String mockInput = "1 3\n3 1\n5 6\n";
         System.setIn(new ByteArrayInputStream(mockInput.getBytes()));
 
-        Map<Integer, ArrayList<Integer>> expected = new HashMap<>();
-        expected.put(1, new ArrayList<>(List.of(3, 3)));
-        expected.put(3, new ArrayList<>(List.of(1, 1)));
-        expected.put(5, new ArrayList<>(List.of(6)));
-        expected.put(6, new ArrayList<>(List.of(5)));
+        Map<Integer, Set<Integer>> expected = new HashMap<>();
+        expected.put(1, new HashSet<>(Set.of(3)));
+        expected.put(3, new HashSet<>(Set.of(1)));
+        expected.put(5, new HashSet<>(Set.of(6)));
+        expected.put(6, new HashSet<>(Set.of(5)));
         //when
-        Map<Integer, ArrayList<Integer>> result = App.getGraphConnections(3, new Scanner(System.in));
+        Map<Integer, Set<Integer>> result = App.getGraphConnectionsMap(3, new Scanner(System.in));
+        //then
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void testGetGraphConnectionsMapWithOneWrongLine() {
+        //given
+        String mockInput = "2\n1 2\n3 h\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(mockInput.getBytes());
+        System.setIn(in);
+        Map<Integer, Set<Integer>> expected = new HashMap<>();
+        expected.put(1, new HashSet<>(Set.of(2)));
+        expected.put(2, new HashSet<>(Set.of(3,1)));
+        expected.put(3, new HashSet<>(Set.of(2)));
+        //when
+        Map<Integer, Set<Integer>> result = App.getGraphConnectionsMap(2, new Scanner(System.in));
         //then
         assertEquals(expected, result);
     }
