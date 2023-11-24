cb399bfc44f2787c97bb15e726caf2c311fd15d5
MarcinKrysinski <59838094+MarcinKrysinski@users.noreply.github.com>
2023-11-24 13:40:05 +0100

    Fix task2 (#4)
    * fix task2
    
    * added test
    
    * refactoring task2

--- task2/src/main/java/pl/krysinski/App.java
+++ task2/src/main/java/pl/krysinski/App.java
@@ -1,52 +1,22 @@
 package pl.krysinski;
 
 import java.util.*;
-import java.util.stream.Collectors;
+
 
 public class App {
 
-    private static final int TARGET_NUMBER = 13;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Enter numbers of integers seperated by a space: ");
         String input = scanner.nextLine();
 
         String[] numbers = input.split(" ");
-        int[] integerArray = getIntegerArray(numbers);
-        List<String> result = findPairs(integerArray);
-        getSortedPairsList(result).forEach(System.out::println);
+        PairFinder pairFinder = new PairFinder(numbers);
+        List<String> result = pairFinder.getAllPairsThatSumUpToTarget(numbers);
+        pairFinder.getSortedPairsList(result).forEach(System.out::println);
+
         scanner.close();
-    }
-
-    public static int[] getIntegerArray(String[] numbers) {
-        try {
-            return Arrays.stream(numbers)
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-        } catch (NumberFormatException e) {
-            System.out.println("Error: Only integers!");
-            System.out.println(e.getMessage());
-            return new int[]{};
-        }
-    }
-
-    static List<String> findPairs(int[] numbersArray) {
-        List<String> result = new ArrayList<>();
-        Set<Integer> checkedNumbers = new HashSet<>();
-        for (int num : numbersArray) {
-            int complement = TARGET_NUMBER - num;
-            if (checkedNumbers.contains(complement)) {
-                int smaller = Math.min(num, complement);
-                int larger = Math.max(num, complement);
-                result.add(smaller + " " + larger);
-            }
-            checkedNumbers.add(num);
-        }
-        return result;
-    }
-
-    static List<String> getSortedPairsList(List<String> result) {
-        return result.stream().sorted().collect(Collectors.toList());
     }
 }
--- task2/src/test/java/pl/krysinski/AppTest.java
+++ task2/src/test/java/pl/krysinski/AppTest.java
@@ -1,41 +1,65 @@
 package pl.krysinski;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class AppTest {
+
+    PairFinder pairFinder = new PairFinder();
+
+
     @Test
     void getIntegerArrayTest() {
         //given
         String[] numbers = {"1", "2", "3", "4"};
         int[] expected = {1, 2, 3, 4};
         //when
-        int[] result = App.getIntegerArray(numbers);
+        int[] result = pairFinder.getIntegerArray(numbers);
         //then
         assertArrayEquals(expected, result);
     }
 
     @Test
-    void findPairsTest() {
+    void getAllPairsThatSumUpToTargetTest() {
         //given
-        int[] numbersArray = {1, 2, 10, 7, 5, 3, 6, 6, 13, 0};
-        List<String> expected = List.of("3 10", "6 7", "6 7", "0 13");
+        String[] numbers = {"1", "2", "10", "5", "3", "6", "6", "13", "0", "7", "7"};
+        List<String> expected = List.of("0 13", "3 10", "6 7", "6 7", "6 7", "6 7");
         //when
-        List<String> result = App.findPairs(numbersArray);
+        List<String> result = pairFinder.getAllPairsThatSumUpToTarget(numbers);
         //then
         assertEquals(expected, result);
     }
 
     @Test
-    void getSortedPairsListTest() {
+    void findPairsTest() {
         //given
-        List<String> noSortedPairsList = List.of("3 10", "6 7", "6 7", "0 13");
-        List<String> expected = List.of( "0 13", "3 10", "6 7", "6 7");
+        Map<Integer, Integer> testMap1 = new HashMap<>();
+        testMap1.put(13, 2);
+        testMap1.put(0, 1);
+        testMap1.put(3, 1);
+        testMap1.put(8, 2);
+        testMap1.put(5, 2);
         //when
-        List<String> result = App.getSortedPairsList(noSortedPairsList);
+        List<String> result = pairFinder.findPairs(testMap1);
+        //then
+        assertEquals(Arrays.asList("0 13", "0 13", "5 8", "5 8", "5 8", "5 8"), result);
+    }
+
+
+    @Test
+    void getSortedPairsListTest() {
+        List<String> noSortedPairsList = List.of("3 10", "6 7", "6 7", "0 13");
+        List<String> expected = List.of("0 13", "3 10", "6 7", "6 7");
+        //when
+        List<String> result = pairFinder.getSortedPairsList(noSortedPairsList);
         //then
         assertEquals(expected, result);
     }
